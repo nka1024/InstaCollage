1ebabbcb8dc372474c05d33f526d73332fce1fc3
Admin <admin@Mac-Admin.local>
2014-09-20 07:09:53 +0400

    Initial Commit

---
+++ InstaCollage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaCollage
+//
+//  Created by Admin on 9/20/14.
+//  Copyright (c) 2014 Kirill Nepomnyaschy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
